b7a03ade6fdf8e25f02c4b63725d0f9006301ab3
Thomas Korrison <tkorrison@agenda21digital.com>
2017-09-19 15:57:17 +0100

    modified insertion sort, all tests passed

--- sorting.c
+++ sorting.c
@@ -1,49 +1,85 @@
 // sorting algorithms
+#include <stdio.h>
+// #include "minunit.h"
+
+// int tests_run = 0;
+
+// void insertion_sort(int input_array[], int length);
+
+
+// static char * test_insertion_sort() 
+// {
+//   int test_data[5] = {6,2,1,3,8};
+//   insertion_sort(test_data, 5);
+
+//   mu_assert("error, test_data[0] not sorted correctedly", test_data[0] == 1);
+//   mu_assert("error, test_data[2] not sorted correctedly", test_data[2] == 3);
+//   mu_assert("error, test_data[4] not sorted correctedly", test_data[4] == 8);
+//   return 0;
+// }
+
+// static char * all_tests()
+// {
+//   mu_run_test(test_insertion_sort);
+//   return 0;
+// }
+
+// int main(int argc, char **argv) {
+//      char *result = all_tests();
+//      if (result != 0) {
+//          printf("%s\n", result);
+//      }
+//      else {
+//          printf("ALL TESTS PASSED\n");
+//      }
+//      printf("Tests run: %d\n", tests_run);
+ 
+//      return result != 0;
+// }
+
 
 // insertion sort
-void insertion_sort(int input_array[])
+void insertion_sort(int input_array[], int length)
 {
-  // loop through each element in an array
-  for (int i = 1; array_length = sizeof(input_array) - 1; i++) 
+  int temp, j;
+  for (int i=1;i<length;i++)
   {
-    // temp store of variables
-    ith_element = input_Array[i];
-    int j = i-1;
-    // loop through each element before the ith_element check if it is larger than the value in front of it.
-    / if it is move it down one place in the array
-    while (j >= 0 and input_array[j] > ith_element)
+    j = i;
+    while (j > 0 && input_array[j] < input_array[j-1])
     {
-      input_array[j+1] = input_array[j];
+      // swap A[j] and A[j-1]
+      temp = input_array[j];
+      input_array[j] = input_array[j-1];
+      input_array[j-1] = temp;
       j--;
     }
-    // insert 
-    input_array[j+1] = ith_element;
   }
 }
 
 // selection sort
-void selection_sort(int input_array[])
-{
-  int size_of_array = sizeof(input_array);
+// void selection_sort(int input_array[])
+// {
+//   int size_of_array = sizeof(input_array);
   
-  // loop through entire array
-  for (int i=0; i < size_of_array-1; i++) 
-  {
-    // update min value as the last element in sorted array
-    int min = input_array[i];
-    // test against other elements in array
-    for (int j=i+1; j < size_of_array; j++) 
-    {
-      // if element is less, update element to the new min value
-      if (input_array[j] < min)
-      {
-        min = input_array[j];
-      }
-    }
-    // if new min found swap with ith value
-    if (min != input_array[i])
-    {
-      swap(min, input_array[i]);
-    }
-  }
-}
+//   // loop through entire array
+//   for (int i=0; i < size_of_array-1; i++) 
+//   {
+//     // update min value as the last element in sorted array
+//     int min = input_array[i];
+//     // test against other elements in array
+//     for (int j=i+1; j < size_of_array; j++) 
+//     {
+//       // if element is less, update element to the new min value
+//       if (input_array[j] < min)
+//       {
+//         min = input_array[j];
+//       }
+//     }
+//     // if new min found swap with ith value
+//     if (min != input_array[i])
+//     {
+//       swap(min, input_array[i]);
+//     }
+//   }
+// }
+
