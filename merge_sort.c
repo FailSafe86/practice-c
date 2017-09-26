46b312fbf67831695968dbf79546b61bcdceeeaf
Thomas Korrison <tkorrison@agenda21digital.com>
2017-09-26 16:42:08 +0100

    added merge sort, tests all passed

---
+++ merge_sort.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <math.h>
+
+/*
+Merge Sort
+==============
+
+https://en.wikipedia.org/wiki/Merge_sort
+
+Worst-case performance: O(n log n)
+Best-case performance: O(n log n) typical
+Average performance: O(n log n)
+
+Worst-case space complexity: O(n) total, O(n) auxiliary
+
+Features
+--------
+- In the worst case, merge sort does about 39% fewer comparisons 
+than quicksort does in the average case
+- is a stable sort
+
+*/
+
+
+void merge(int arr[], int left_idx, int right_idx, int end_idx);
+
+//merge sort on an array
+void merge_sort(int arr[], int left_idx, int right_idx)
+{
+  if (left_idx < right_idx)
+  {
+    int mid_idx = floor((left_idx + right_idx) / 2);
+    merge_sort(arr, left_idx, mid_idx);
+    merge_sort(arr, mid_idx+1, right_idx);
+    merge(arr, left_idx, mid_idx, right_idx);
+  }
+}
+
+
+// merging an array
+void merge(int arr[], int left_idx, int mid_idx, int right_idx)
+{
+  // length of each subarray
+  int left_length = mid_idx - left_idx + 1;
+  int right_length = right_idx - mid_idx;
+
+  // define the left and right sub-arrays
+  int left_arr[left_length];
+  int right_arr[right_length];
+
+  // copy sub-array arr[left...mid] into left_arr[0...left_length]
+  for (int i = 0; i < left_length; i++)
+  {
+    left_arr[i] = arr[left_idx + i];
+  }
+  // copy sub-array arr[mid + 1...right] into right_arr[0...right_length]
+  for (int j = 0; j < right_length; j++)
+  {
+    right_arr[j] = arr[mid_idx + j + 1];
+  }
+
+  // sentinals
+  //left_arr[left_length+1] = 99;
+  //right_arr[right_length+1] = 99;
+  int i = 0;
+  int j = 0;
+
+  for (int k = left_idx; k <= right_idx; k++)
+  {
+    if (left_arr[i] <= right_arr[j])
+    {
+      arr[k] = left_arr[i];
+      i++;
+    }
+    else
+    {
+      arr[k] = right_arr[j];
+      j++;
+    }
+  }
+}
--- tests.c
+++ tests.c
@@ -5,6 +5,8 @@
 #include "selection_sort.c"
 #include "binary_search.c"
 #include "quicksort.c"
+#include "merge_sort.c"
+
 
 int tests_run = 0;
 
@@ -13,6 +15,8 @@ void insertion_sort_with_swap(int input_array[], int length);
 void insertion_sort_recursive(int arr[], int length);
 void selection_sort(int input_array[], int length);
 void quicksort(int arr[], int p, int r);
+void merge_sort(int arr[], int left_idx, int right_idx);
+
 bool binary_search(int key, int array[], int min, int max);
 
 //tests on sorts
@@ -21,12 +25,13 @@ static char * test_insertion_sort()
   int test_data[5] = {6,2,1,3,8};
   insertion_sort(test_data, 5);
 
-  printf("Test: quicksort...\n");
-  mu_assert("error: test_data[0] not sorted correctedly", test_data[0] == 1);
-  mu_assert("error: test_data[1] not sorted correctedly", test_data[1] == 2);
-  mu_assert("error: test_data[2] not sorted correctedly", test_data[2] == 3);
-  mu_assert("error: test_data[3] not sorted correctedly", test_data[3] == 6);
-  mu_assert("error: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("Test: quicksort...");
+  mu_assert("\nerror: test_data[0] not sorted correctedly", test_data[0] == 1);
+  mu_assert("\nerror: test_data[1] not sorted correctedly", test_data[1] == 2);
+  mu_assert("\nerror: test_data[2] not sorted correctedly", test_data[2] == 3);
+  mu_assert("\nerror: test_data[3] not sorted correctedly", test_data[3] == 6);
+  mu_assert("\nerror: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("all Tests Passed\n");
   return 0;
 }
 
@@ -35,12 +40,13 @@ static char * test_insertion_sort_with_swap()
   int test_data[5] = {6,2,1,3,8};
   insertion_sort_with_swap(test_data, 5);
 
-  printf("Test: insertion_sort_with_swap...\n");
-  mu_assert("error: test_data[0] not sorted correctedly", test_data[0] == 1);
-  mu_assert("error: test_data[1] not sorted correctedly", test_data[1] == 2);
-  mu_assert("error: test_data[2] not sorted correctedly", test_data[2] == 3);
-  mu_assert("error: test_data[3] not sorted correctedly", test_data[3] == 6);
-  mu_assert("error: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("Test: insertion_sort_with_swap...");
+  mu_assert("\nerror: test_data[0] not sorted correctedly", test_data[0] == 1);
+  mu_assert("\nerror: test_data[1] not sorted correctedly", test_data[1] == 2);
+  mu_assert("\nerror: test_data[2] not sorted correctedly", test_data[2] == 3);
+  mu_assert("\nerror: test_data[3] not sorted correctedly", test_data[3] == 6);
+  mu_assert("\nerror: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("all Tests Passed\n");
   return 0;
 }
 
@@ -49,12 +55,13 @@ static char * test_insertion_sort_recursive()
   int test_data[5] = {6,2,1,3,8};
   insertion_sort_recursive(test_data, 5);
 
-  printf("Test: test_insertion_sort_recursive...\n");
-  mu_assert("error: test_data[0] not sorted correctedly", test_data[0] == 1);
-  mu_assert("error: test_data[1] not sorted correctedly", test_data[1] == 2);
-  mu_assert("error: test_data[2] not sorted correctedly", test_data[2] == 3);
-  mu_assert("error: test_data[3] not sorted correctedly", test_data[3] == 6);
-  mu_assert("error: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("Test: test_insertion_sort_recursive...");
+  mu_assert("\nerror: test_data[0] not sorted correctedly", test_data[0] == 1);
+  mu_assert("\nerror: test_data[1] not sorted correctedly", test_data[1] == 2);
+  mu_assert("\nerror: test_data[2] not sorted correctedly", test_data[2] == 3);
+  mu_assert("\nerror: test_data[3] not sorted correctedly", test_data[3] == 6);
+  mu_assert("\nerror: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("all Tests Passed\n");
   return 0;
 }
 
@@ -63,12 +70,13 @@ static char * test_selection_sort()
   int test_data[5] = {6,2,1,3,8};
   selection_sort(test_data, 5);
 
-  printf("Test: selection_sort...\n");
-  mu_assert("error: test_data[0] not sorted correctedly", test_data[0] == 1);
-  mu_assert("error: test_data[1] not sorted correctedly", test_data[1] == 2);
-  mu_assert("error: test_data[2] not sorted correctedly", test_data[2] == 3);
-  mu_assert("error: test_data[3] not sorted correctedly", test_data[3] == 6);
-  mu_assert("error: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("Test: selection_sort...");
+  mu_assert("\nerror: test_data[0] not sorted correctedly", test_data[0] == 1);
+  mu_assert("\nerror: test_data[1] not sorted correctedly", test_data[1] == 2);
+  mu_assert("\nerror: test_data[2] not sorted correctedly", test_data[2] == 3);
+  mu_assert("\nerror: test_data[3] not sorted correctedly", test_data[3] == 6);
+  mu_assert("\nerror: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("all Tests Passed\n");
   return 0;
 }
 
@@ -77,25 +85,47 @@ static char * test_quicksort()
   int test_data[5] = {6,2,1,3,8};
   quicksort(test_data, 0, 4);
 
-  printf("Test: quicksort...\n");
-  mu_assert("error: test_data[0] not sorted correctedly", test_data[0] == 1);
-  mu_assert("error: test_data[1] not sorted correctedly", test_data[1] == 2);
-  mu_assert("error: test_data[2] not sorted correctedly", test_data[2] == 3);
-  mu_assert("error: test_data[3] not sorted correctedly", test_data[3] == 6);
-  mu_assert("error: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("Test: quicksort...");
+  mu_assert("\nerror: test_data[0] not sorted correctedly", test_data[0] == 1);
+  mu_assert("\nerror: test_data[1] not sorted correctedly", test_data[1] == 2);
+  mu_assert("\nerror: test_data[2] not sorted correctedly", test_data[2] == 3);
+  mu_assert("\nerror: test_data[3] not sorted correctedly", test_data[3] == 6);
+  mu_assert("\nerror: test_data[4] not sorted correctedly", test_data[4] == 8);
+  printf("all Tests Passed\n");
+  return 0;
+}
+
+static char * test_merge_sort() 
+{
+  printf("Test: merge_sort...");
+  
+  int test_data[5] = {6,2,1,3,8};
+  merge_sort(test_data, 0, 4);
+  mu_assert("\nerror: test_data[0] not sorted correctedly", test_data[0] == 1);
+  mu_assert("\nerror: test_data[1] not sorted correctedly", test_data[1] == 2);
+  mu_assert("\nerror: test_data[2] not sorted correctedly", test_data[2] == 3);
+  mu_assert("\nerror: test_data[3] not sorted correctedly", test_data[3] == 6);
+  mu_assert("\nerror: test_data[4] not sorted correctedly", test_data[4] == 8);
+
+  int test_data2[10] = {9,8,7,6,5,5,4,3,2,1};
+  merge_sort(test_data2, 0, 9);
+  mu_assert("\nerror: test_data2[0] not sorted correctedly", test_data2[0] == 1);
+  mu_assert("\nerror: test_data2[5] not sorted correctedly", test_data2[5] == 5);
+  mu_assert("\nerror: test_data2[9] not sorted correctedly", test_data2[9] == 9);
+  
+  printf("all Tests Passed\n");
   return 0;
 }
 
 
-// tests on searches
 static char * test_binary_search()
 {
   int test_data[5] = {6,2,1,3,8};
-  binary_search(8, test_data, 0, 4);
 
-  //printf("Testing: binary_search...")
-  mu_assert("error: binary_search", binary_search(8, test_data, 0, 4) == true);
-  mu_assert("error: binary_search, the number 7 is not present in array", binary_search(7, test_data, 0, 4) == false);
+  printf("Test: binary_search...");
+  mu_assert("\nerror: binary_search", binary_search(8, test_data, 0, 4) == true);
+  mu_assert("\nerror: binary_search, the number 7 is not present in array", binary_search(7, test_data, 0, 4) == false);
+  printf("all Tests Passed\n");
   return 0;
 }
 
@@ -108,6 +138,7 @@ static char * all_tests()
   //mu_run_test(test_insertion_sort_recursive);
   mu_run_test(test_selection_sort);
   mu_run_test(test_quicksort);
+  mu_run_test(test_merge_sort);
   mu_run_test(test_binary_search);
   return 0;
 }
